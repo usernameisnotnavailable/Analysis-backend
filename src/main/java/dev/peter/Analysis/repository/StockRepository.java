77e337b6226273aacfb3465ee4e061e4ba9d1837
Peter Szabo <peter_szabo90@hotmail.com>
2024-09-16 22:47:21 +0200

    minor change in tests

--- src/main/java/dev/peter/Analysis/repository/StockRepository.java
+++ src/main/java/dev/peter/Analysis/repository/StockRepository.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.LinkedList;
+import java.util.List;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
     LinkedList<Stock> getStocksByCompanyNameAndTradeDateBetween(String companyName, LocalDate startDate, LocalDate endDate);
+
 
 
 }
